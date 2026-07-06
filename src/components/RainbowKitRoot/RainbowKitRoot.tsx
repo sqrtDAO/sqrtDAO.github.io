15eb6137df193244277791b6aed3745a2f19cdd9
Ali Ghahremani <alighahremani1377@gmail.com>
2026-07-06 18:18:24 +0330

    setup: rainbowkit, viem, wagmi

---
+++ src/components/RainbowKitRoot/RainbowKitRoot.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { WagmiProvider } from "wagmi";
+import { RainbowKitProvider, darkTheme } from "@rainbow-me/rainbowkit";
+import { getDefaultConfig } from "@rainbow-me/rainbowkit";
+import { mainnet, sepolia } from "wagmi/chains";
+import { useState, type ReactNode } from "react";
+import "@rainbow-me/rainbowkit/styles.css";
+
+
+const config = getDefaultConfig({
+  appName: "sqrtDAO",
+  projectId: "af0315795cabd9f168cf79b92e96863a",
+  chains: [mainnet, sepolia],
+  ssr: true,
+});
+
+export default function RainbowKitRoot({ children }: { children: ReactNode }) {
+  const [queryClient] = useState(() => new QueryClient());
+
+  return (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <RainbowKitProvider
+          theme={darkTheme({
+            accentColor: "#6366f1",
+            borderRadius: "medium",
+          })}
+          modalSize="compact"
+        >
+          {children}
+        </RainbowKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  );
+}
