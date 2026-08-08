d3bcf73bbd0742e6de49254d3077a48c5e06400f
Ali Ghahremani <alighahremani1377@gmail.com>
2026-08-08 22:50:01 +0330

    fix: rpc error by overwriting sepolia configuration

--- src/components/RainbowKitRoot/RainbowKitRoot.tsx
+++ src/components/RainbowKitRoot/RainbowKitRoot.tsx
@@ -4,9 +4,43 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { WagmiProvider } from "wagmi";
 import { RainbowKitProvider, darkTheme } from "@rainbow-me/rainbowkit";
 import { getDefaultConfig } from "@rainbow-me/rainbowkit";
-import { sepolia } from "wagmi/chains";
+// import { sepolia } from "wagmi/chains";
 import { useState, type ReactNode } from "react";
 import "@rainbow-me/rainbowkit/styles.css";
+import { defineChain } from "viem";
+
+const sepolia = /*#__PURE__*/ defineChain({
+  id: 11_155_111,
+  name: "Sepolia",
+  nativeCurrency: { name: "Sepolia Ether", symbol: "ETH", decimals: 18 },
+  rpcUrls: {
+    default: {
+      http: [
+        "https://sepolia.gateway.tenderly.co",
+        "https://sepolia.drpc.org",
+        "https://ethereum-sepolia-rpc.publicnode.com",
+      ],
+    },
+  },
+  blockExplorers: {
+    default: {
+      name: "Etherscan",
+      url: "https://sepolia.etherscan.io",
+      apiUrl: "https://api-sepolia.etherscan.io/api",
+    },
+  },
+  contracts: {
+    multicall3: {
+      address: "0xca11bde05977b3631167028862be2a173976ca11",
+      blockCreated: 751532,
+    },
+    ensUniversalResolver: {
+      address: "0xeeeeeeee14d718c2b47d9923deab1335e144eeee",
+      blockCreated: 8_928_790,
+    },
+  },
+  testnet: true,
+});
 
 const config = getDefaultConfig({
   appName: "sqrtDAO",
