9deac9516bae2f1002ffa0f4812290aa5bdf70ed
Ali Ghahremani <alighahremani1377@gmail.com>
2026-08-09 17:59:26 +0330

    fix: adding await to Wizard confirm so error can be handled

--- src/components/DistributionWizard/DistributionWizard.tsx
+++ src/components/DistributionWizard/DistributionWizard.tsx
@@ -371,7 +371,7 @@ export default function DistributionWizard(props: {
         minParS !== "" ? minParS : "0",
         pTokenDecimals,
       );
-      props.onFinish({
+      await props.onFinish({
         totalDistributionAmount:
           totalDistributionAmountN - initialDistributionLiquidityN,
         participationToken: participationToken.address,
